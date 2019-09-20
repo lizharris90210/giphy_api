93acc8d1f62bc4bd3ad39e0527f715f094a7b843
Liz Harris <lizharris90210@gmail.com>
2019-09-19 22:27:32 -0400

    search works, add search to topic buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,78 @@
 // call jquery
 $(document).ready(function() {
+
+  var topics = [
+    "Guitar",
+    "Piano",
+    "Drums",
+    "Clarinet",
+    "Tuba",
+    "Saxaphone",
+    "Flute",
+    "Banjo",
+    "Trumpet",
+    "Ukulele",
+    "Oboe",
+    "Trombone",
+    "Tambourine",
+    "Harp",
+    "Violin",
+    "Stand-up Bass",
+    "Xylophone",
+    "Glockenspiel",
+    "Harmonica"
+  ];
+
+  for(var i= 0; i < topics.length; i++) {
+    var button = $("<button>").text(topics[i]);
+    $("#music").append(button)
+  }
+  // add functionality
+
+
   // Event listener for searchButton
   $("#searchButton").on("click", function() {
     // Storing our giphy API URL
-    var apiKey = ju6uCnRv4cMKILzG5m62vt52JavnVngT;
+    var apiKey = "ju6uCnRv4cMKILzG5m62vt52JavnVngT";
 
-    // music topics
-    var topics = [
-      "Guitar",
-      "Piano",
-      "Drums",
-      "Clarinet",
-      "Tuba",
-      "Saxaphone",
-      "Flute",
-      "Banjo",
-      "Trumpet",
-      "Ukulele",
-      "Oboe",
-      "Trombone",
-      "Tambourine",
-      "Harp",
-      "Violin",
-      "Stand-up Bass",
-      "Xylophone",
-      "Glockenspiel",
-      "Harmonica"
-    ];
+    var userSearch = $("#searchInput").val();
 
     var queryURL =
-      "https://api.giphy.com/v1/gifs/random?api_key=" + music + apiKey;
-    
-    var userSearch = $(this).attr("topics");
+      "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + userSearch + "&limit=10";
 
     // AJAX GET request to queryURL
     $.ajax({
       url: queryURL,
       method: "GET"
-    });
-
-    // After the data from the AJAX request comes back
-    then(function(response) {
-      console.log(queryURL);
+    })
+    .then(function(response) {
       // Saving the image_original_url property
-      var imageUrl = response.data;
+      var results = response.data;
+      console.log(results)
       for (var i = 0; i < results.length; i++) {
         // Only taking action if the photo has an appropriate rating
         if (results[i].rating !== "r") {
           // Creating a div for the gif
           var gifDiv = $("<div>");
 
-          // Storing the result item's rating
+          // Storing result rating
           var rating = results[i].rating;
 
-          // Creating a paragraph tag with the result item's rating
+          // Create paragraph tag with result rating
           var p = $("<p>").text("Rating: " + rating);
 
           // Creating an image tag
           var gifImage = $("<img>");
 
-          // Giving the image tag an src attribute of a proprty pulled off the
-          // result item
+          // Giving image tag src attribute of proporty pulled from result item
           gifImage.attr("src", results[i].images.fixed_height.url);
 
-          // Appending the paragraph and personImage we created to the "gifDiv" div we created
+          // Appending the paragraph and gifImage created to the "gifDiv" created
           gifDiv.append(p);
           gifDiv.append(gifImage);
 
-          // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-          $("#giphyImges").prepend(gifDiv);
+          // Prepending gifDiv to "#giphyImages" div HTML
+          $("#giphyImages").prepend(gifDiv);
         }
       }
     });
