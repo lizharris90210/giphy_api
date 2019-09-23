3a487b211daceadba01059dc26a4960559372231
Liz Harris <lizharris90210@gmail.com>
2019-09-22 22:08:54 -0400

    search input buttons almost working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,136 +1,122 @@
-// call jquery
+var topics = [
+  "Guitar",
+  "Piano",
+  "Drums",
+  "Clarinet",
+  "Tuba",
+  "Saxaphone",
+  "Flute",
+  "Banjo",
+  "Trumpet",
+  "Ukulele",
+  "Oboe",
+  "Trombone",
+  "Tambourine",
+  "Harp",
+  "Violin"
+];
+
 $(document).ready(function() {
-
-  var topics = [
-    "Guitar",
-    "Piano",
-    "Drums",
-    "Clarinet",
-    "Tuba",
-    "Saxaphone",
-    "Flute",
-    "Banjo",
-    "Trumpet",
-    "Ukulele",
-    "Oboe",
-    "Trombone",
-    "Tambourine",
-    "Harp",
-    "Violin",
-    "Stand-up Bass"
-  ];
-
-
-// makes var topics into buttons
-  for(var i= 0; i < topics.length; i++) {
-    var button = $("<button>").text(topics[i]).addClass('instrument').attr({'data-name': topics[i]});
-    $("#music").append(button)
+  for (var i = 0; i < topics.length; i++) {
+    var button = $("<button>")
+      .text(topics[i])
+      .addClass("instrument")
+      .attr({ id: topics[i] });
+    $("#music").append(button);
   }
 
-  // !!!buttons clicked won't search button text!!!
-  $(".instrument").on("click", function()  {
-
-    var thisButton = $('#instrument').val();
-
+  $(".instrument").on("click", function() {
+    console.log(this.id);
+    var thisButton = this.id;
     var apiKey = "ju6uCnRv4cMKILzG5m62vt52JavnVngT";
-
     var queryURL =
-    "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + thisButton + "&limit=10";
-  
+      "https://api.giphy.com/v1/gifs/search?api_key=" +
+      apiKey +
+      "&q=" +
+      thisButton +
+      "&limit=5";
 
-     $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    .done(function(giphy) {
-      currentGif = giphy.data;
-      $.each(currentGif, function(button,value){
-      animatedGif= value.images.original.url;
-      pausedGif = value.images.original_still.url;
-    })})
-
-    .then(function(response) {
-      // Saving the image_original_url property
-      var results = response.data;
-      console.log(results)
-      for (var i = 0; i < results.length; i++) {
-        // Only taking action if the photo has an appropriate rating
-        if (results[i].rating !== "r") {
-          // Creating a div for the gif
-          var gifDiv = $("<div>");
-
-          // Storing result rating
-          var rating = results[i].rating;
-
-          // Create paragraph tag with result rating
-          var p = $("<p>").text("Rating: " + rating);
-
-          // Creating an image tag
-          var gifImage = $("<img>");
-
-          // Giving image tag src attribute of proporty pulled from result item
-          gifImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and gifImage created to the "gifDiv" created
-          gifDiv.append(p);
-          gifDiv.append(gifImage);
-
-          // Prepending gifDiv to "#giphyImages" div HTML
-          $("#giphyImages").prepend(gifDiv);
-        }
-      }
-  })
-})
-
-  
-       
-  // add functionality
-  // make gifs static, click to active, click to static
-  // Event listener for searchButton
-  $("#searchButton").on("click", function() {
-    // Storing our giphy API URL
-    var apiKey = "ju6uCnRv4cMKILzG5m62vt52JavnVngT";
-
-    var userSearch = $("#searchInput").val();
-
-    var queryURL =
-      "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + userSearch + "&limit=10";
-    
-    // AJAX GET request to queryURL
     $.ajax({
       url: queryURL,
       method: "GET"
     })
     .then(function(response) {
-      // Saving the image_original_url property
       var results = response.data;
-      console.log(results)
+      console.log(results);
+
       for (var i = 0; i < results.length; i++) {
-        // Only taking action if the photo has an appropriate rating
         if (results[i].rating !== "r") {
-          // Creating a div for the gif
           var gifDiv = $("<div>");
-
-          // Storing result rating
           var rating = results[i].rating;
-
-          // Create paragraph tag with result rating
           var p = $("<p>").text("Rating: " + rating);
-
-          // Creating an image tag
           var gifImage = $("<img>");
-
-          // Giving image tag src attribute of proporty pulled from result item
           gifImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and gifImage created to the "gifDiv" created
           gifDiv.append(p);
           gifDiv.append(gifImage);
-
-          // Prepending gifDiv to "#giphyImages" div HTML
           $("#giphyImages").prepend(gifDiv);
         }
       }
     });
+  });
+});
+var topics = [
+  "Guitar",
+  "Piano",
+  "Drums",
+  "Clarinet",
+  "Tuba",
+  "Saxaphone",
+  "Flute",
+  "Banjo",
+  "Trumpet",
+  "Ukulele",
+  "Oboe",
+  "Trombone",
+  "Tambourine",
+  "Harp",
+  "Violin"
+];
+$("#searchButton").on("click", function() {
+  var apiKey = "ju6uCnRv4cMKILzG5m62vt52JavnVngT";
+  var userSearch = $("#searchInput").val();
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=" +
+    apiKey +
+    "&q=" +
+    userSearch +
+    "&limit=5";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var results = response.data;
+    for (var i = 0; i < results.length; i++) {
+      // !!!buttons show up but not search input text and not functional!!!
+      var input = $("#searchInput");
+      var button = $("<button>")
+        .text(input)
+        .addClass("instrument")
+        .attr({ id: topics[i] });
+      $("#music").append(button);
+
+      if (results[i].rating !== "r") {
+        var gifDiv = $("<div>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var gifImage = $("<img>");
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+        $("#giphyImages").prepend(gifDiv);
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-animate", results[i].images.fixed_height.url);
+        gifImage.attr("data-state", "still");
+        gifImage.prepend(p);
+        gifDiv.prepend(showImage);
+        gifDiv.prepend(instrumentImage);
+        $("#music").append(gifDiv);
+      }
+    }
   });
 });
